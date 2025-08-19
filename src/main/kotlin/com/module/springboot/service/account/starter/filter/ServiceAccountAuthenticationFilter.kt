8dba593a623605af2c2224decb14d6313f3f426f
William Chen <william_w_chen@trendmicro.com>
2025-08-19 10:27:28 +0800

    Remove @Component annotation from ServiceAccountAuthenticationFilter to simplify the class structure

--- src/main/kotlin/com/module/springboot/service/account/starter/filter/ServiceAccountAuthenticationFilter.kt
+++ src/main/kotlin/com/module/springboot/service/account/starter/filter/ServiceAccountAuthenticationFilter.kt
@@ -10,11 +10,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
-import org.springframework.stereotype.Component
 import org.springframework.util.AntPathMatcher
 import org.springframework.web.filter.OncePerRequestFilter
 
-@Component
 class ServiceAccountAuthenticationFilter(
     private val serviceAccountJwtService: ServiceAccountJwtService,
     private val serviceAccountSecurityProperties: ServiceAccountSecurityProperties
